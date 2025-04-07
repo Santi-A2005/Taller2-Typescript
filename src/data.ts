481f47f2a05a27696c4cd5c726c8a5c391a7eba4
Santiago Alvarez <s.alvarez1123@uniandes.edu.co>
2025-04-06 19:14:50 -0500

    Correccion y fin

--- src/data.ts
+++ src/data.ts
@@ -6,7 +6,7 @@ export const series: Serie[] = [
     "Breaking Bad", 
     "AMC", 
     5, 
-    "Set and filmed in Albuquerque, New Mexico...",
+    "Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer",
      "https://www.amc.com/shows/breaking-bad", 
      "https://i.imgur.com/GGje0vc.jpg"
     ),
